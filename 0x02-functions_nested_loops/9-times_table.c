0b40d150e72a2eb9f8dbfc9741a1854ed3152a64
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-09-13 13:26:32 -0700

    9x table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,11 @@
 void times_table(void)
 {
 int x, y, z, u, d;
-fir (x = 0; x <= 9; x++)
+for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
-z = x + y;
+z = x * y;
 if (z > 9)
 {
 u = z % 10;
@@ -36,3 +36,4 @@ _putchar(z + '0');
 _putchar('\n');
 }
 }
+
