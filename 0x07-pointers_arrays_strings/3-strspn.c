6f3c00f02cbf7c3d62d101fe28383ca20d46aebe
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-09-25 13:13:55 -0700

    fuction that reada the length of a substring prefix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,10 +12,13 @@ unsigned int _strspn(char *s, char *accept)
 	int i;
 	int k;
 
-	for (i = 0; s[i] != accept[k]; k++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		for (k = 0; s[i] != accept[k]; k++)
+		{
 		if (accept[k] == '\0')
 			return (i);
+		}
 	}
 	return (0);
 }
