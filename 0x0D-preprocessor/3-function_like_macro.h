91499f4a2956a50973113fb19129f008319878d0
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-10-10 14:26:55 -0700

    a function that print absolute value

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File - 3-function_like_macro.h
+ *
+ * @x: input value
+ * Description: header file for absolute value
+ */
+#define ABS(x) ((x) < 0 ? (-1) * (x): (x))
+
+#endif
