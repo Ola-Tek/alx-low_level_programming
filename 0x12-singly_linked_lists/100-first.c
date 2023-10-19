5c6eea2a9a4c319ec2b3d50f30c5fc128684e858
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-10-19 16:04:16 -0700

    a function that is executed first before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void comeFirstByOla(void)__attribute__((constructor));
+/**
+ * comeFirstByOla - the function comes first
+ */
+void comeFirstByOla(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
