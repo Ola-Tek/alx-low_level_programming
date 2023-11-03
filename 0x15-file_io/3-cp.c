a71db10313b57c77f882001fa34e6a3396608ac0
Obisesan Olabode <Obisesanolabode10@gmail.com>
2023-11-03 10:59:56 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,14 +19,14 @@ int main(int argc, char *argv[])
 	}
 
 	src = open(argv[1], O_RDONLY);
-	check_IO_stat(src, -1, argv[1], '0');
+	check_IO_stat(src, -1, argv[1], 'O');
 	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 	check_IO_stat(dest, -1, argv[2], 'W');
 	while (n_read == 1024)
 	{
 		n_read = read(src, buffer, sizeof(buffer));
 		if (n_read == -1)
-			check_IO_stat(-1, -1, argv[1], '0');
+			check_IO_stat(-1, -1, argv[1], 'O');
 		wrote = write(dest, buffer, n_read);
 		if (wrote == -1)
 			check_IO_stat(-1, -1, argv[2], 'W');
@@ -53,7 +53,7 @@ void check_IO_stat(int stat, int fd, char *filename, char mode)
 		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
 		exit(100);
 	}
-	else if (mode == '0' && stat == -1)
+	else if (mode == 'O' && stat == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", filename);
 		exit(98);
